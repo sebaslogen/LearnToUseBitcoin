ca43940bc0ff418b214a2314d422d4612d7b5dbf
Sebas <neoranga55@gmail.com>
2014-03-02 14:46:53 +0000

    Missing Wallet model data validation

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -4,4 +4,9 @@ class Wallet < ActiveRecord::Base
             uniqueness: true,
             #format: { with: /regex/i },
             length: { minimum: 5, maximum: 150 }  ######################################################## TODO: Test code
+  validates :private_key,
+            presence: true,
+            uniqueness: true,
+            #format: { with: /regex/i },
+            length: { minimum: 5, maximum: 150 }  ######################################################## TODO: Test code
 end
