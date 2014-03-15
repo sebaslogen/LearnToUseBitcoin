920a206e00c76577606e8abcbcf4f306390e0eb7
Sebas <neoranga55@gmail.com>
2014-03-15 20:30:53 +0000

    Show demo content section using AngularJS controller with animation

---
+++ app/assets/javascripts/angular/controllers/demo.js
@@ -0,0 +1,7 @@
+function DemoCtrl ($scope) {
+  $scope.showDemo = function() {
+    $('#show-demo').fadeOut("slow", function() {
+      $('#show-demo').replaceWith($("#demo-content").fadeIn("slow"));
+    });
+  }
+}
