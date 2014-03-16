9f28ffe220de04ab1912c40fc101fd252147cbdb
Sebas <neoranga55@gmail.com>
2014-03-16 13:39:05 +0000

    Add all content to first demo point

--- app/assets/javascripts/angular/controllers/demo.js
+++ app/assets/javascripts/angular/controllers/demo.js
@@ -2,6 +2,7 @@ function DemoCtrl ($scope) {
   $scope.showDemo = function() {
     $('#show-demo').fadeOut("slow", function() {
       $('#show-demo').replaceWith($("#demo-content").fadeIn("slow"));
+      $('div#start').height(parseInt($('#demo-content').height()) + 100); // Fix section's height
     });
   }
 }
