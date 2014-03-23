27ac237fa90a22f5939aa8430dadd1b25e1a2042
Sebas <neoranga55@gmail.com>
2014-03-23 13:34:17 +0000

    Add interactive transaction demo bitcoins available counter

--- app/assets/javascripts/angular/controllers/transaction.js
+++ app/assets/javascripts/angular/controllers/transaction.js
@@ -2,4 +2,15 @@ function TransactionCtrl ($scope) {
   $scope.transactionDemo = function() {
     console.log("Started transaction demo");
   }
+  /*$scope.$watch('input_amount', function() {
+    
+  }, true);*/
+  $scope.remainingBitcoins = function() {
+    console.log("calculating");
+    if (parseInt()) {
+      return 1 - $scope.input_amount;
+    } else {
+      return 1;
+    }
+  }
 }
