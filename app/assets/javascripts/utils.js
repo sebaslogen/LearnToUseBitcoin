aae94f9fb59c9c6884e989125a96fc3cf691ac0d
Sebas <neoranga55@gmail.com>
2014-04-13 21:29:34 +0000

    Add countries to list of Bitcoin sellers

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -17,13 +17,26 @@ function detectLocation() {
 }
 
 function loadLocalBitcoinSellers() {
+  $('#bitcoin-sellers').children().remove(); // Remove any previous links (e.g. on IP/country change)
   var cc = user_location.country_code;
-  if (cc == 'NL' || (cc == 'BE')) {
-    $('#bitcoin-sellers').append('<a href="https://www.Bitonic.nl" target="_blank"> Bitonic.nl </a>');
+  if (cc == 'NL' || (cc == 'BE') || (cc == 'LU')) {
+    $('#bitcoin-sellers').append('<a href="https://bitonic.nl/?refcode=8w0UlJ123111" target="_blank"> Bitonic </a>');
     $('#bitcoin-sellers').append('<a href="https://www.happycoins.com/Account/Index/eb754703-5a33-46fd-b0d2-9a2bb7e465e3" target="_blank"> HappyCoins </a>');
   }
-  if (cc == 'UK' || cc == 'SE' || cc == 'NL' || cc == 'DE' || cc == 'FR' || cc == 'IT' || cc == 'ES' || cc == 'PL' || (cc == 'HU') ) {
-    $('#bitcoin-sellers').append('<a href="https://safello.com/" target="_blank"> Safello </a>');
+  if (cc == 'GB' || cc == 'SE' || cc == 'NL' || cc == 'DE' || cc == 'FR' || cc == 'IT' || cc == 'ES' || 
+      cc == 'PL' || (cc == 'HU') ) {
+    $('#bitcoin-sellers').append('<a href="https://safello.com/buy" target="_blank"> Safello </a>');
+  }
+  if (cc == 'GB' || cc == 'SE' || cc == 'NL' || cc == 'DE' || cc == 'FR' || cc == 'IT' || cc == 'ES' || 
+      cc == 'PL' || (cc == 'HU') || (cc == 'PT') || (cc == 'LI') || (cc == 'LU') || (cc == 'BE') || (cc == 'AT') ) {
+    $('#bitcoin-sellers').append('<a href="https://bittylicious.com/" target="_blank"> Bittylicious </a>');
+  }
+  if (cc == 'RU') {
+    $('#bitcoin-sellers').append('<a href="https://btc-e.com" target="_blank"> BTC-e </a>');
+  }
+  if (cc == 'US') {
+    $('#bitcoin-sellers').append('<a href="https://coinbase.com/?r=52ac992c01478f0e730000b9&utm_campaign=user-referral&src=referral-link" target="_blank"> coinbase </a>');
+    $('#bitcoin-sellers').append('<a href="http://www.expresscoin.com/" target="_blank"> expresscoin </a>');
   }
 }
 
