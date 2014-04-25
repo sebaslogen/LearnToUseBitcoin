f1de6d5c526b77f927c7c3e365c0bfc2af456ca3
Sebas <neoranga55@gmail.com>
2014-04-25 20:13:53 +0000

    Change 'What is Bitcoin?' video with newer version

--- app/assets/javascripts/common/animation.js
+++ app/assets/javascripts/common/animation.js
@@ -7,8 +7,54 @@ function setupAutoScroll() {
       if ($(window).scrollTop() + $( window ).height() <= parseInt($('#welcome').css('height')) + 100) {
         autoscrolled = true;
         moveTo('#what', 3000);  // Move automaticaly but slowly
+        analytics.track('AutoScrolled to Welcome section');
       }
-      analytics.track('AutoScrolled to Welcome section');
     }, 4000); // Wait 4 seconds to automatically move
   }
+}
+
+
+
+/* Youtube video control functions */
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('youtube-video', {
+    events: {
+      'onStateChange': onPlayerStateChange,
+      'onError': onPlayerError
+    }
+  });
+}
+
+// The API calls this function when the player's state changes to playing
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    setTimeout(autoScrollOnVideoFinish, 1000);
+    player.auto_moving = true;
+  }
+}
+
+// Auto-scroll to next section in the last seconds of the video or when it´s finished
+function autoScrollOnVideoFinish() {
+  if (player != null) {
+    if ( (player.getDuration() - player.getCurrentTime() <= 3.5 ) ||
+       (player.getPlayerState() == YT.PlayerState.ENDED) ) {
+      player = null;
+      moveTo('#start', 3000);
+    } else {
+      setTimeout(autoScrollOnVideoFinish, 300);
+    }
+  }
+}
+
+function onPlayerError() {
+  moveTo('#start', 2000);
 }
