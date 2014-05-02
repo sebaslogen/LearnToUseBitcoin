e5feb8f965cb65dfbaa90b2bbf07b11d6a9ef6ee
Sebas <neoranga55@gmail.com>
2014-05-02 19:40:48 +0000

    First translated texts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < HighVoltage::PagesController
   layout :layout_for_page
+  before_filter :set_locale
 
+  private
+    def set_locale
+      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+    end
+  
   private
 
   def layout_for_page
