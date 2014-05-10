830dbbb5882f17fe6095c15e7b429724290bd118
Sebas <neoranga55@gmail.com>
2014-05-10 14:53:12 +0000

    Load demo content when requested earlier than expected

--- app/assets/javascripts/common/angular/controllers/demo.js
+++ app/assets/javascripts/common/angular/controllers/demo.js
@@ -1,5 +1,8 @@
 ltubApp.controller('DemoCtrl', ['$scope', function($scope) {
   $scope.showDemo = function() {
+    if ( $("#demo-content").hasClass('empty-content') ) {
+      loadDemoContent(); // Load immediately if requested earlier than expected
+    }
     $('#show-demo').fadeOut("slow", function() {
       $('div.sStart').height('100%'); // Change from fixed to auto adjust height after load
       $('#show-demo').replaceWith($("#demo-content").fadeIn("slow"));
