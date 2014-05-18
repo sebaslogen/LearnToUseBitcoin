086b057b5003e6d8212f10fda6154383e28f71cd
Sebas <neoranga55@gmail.com>
2014-05-18 19:13:56 +0000

    Update translations and optimize to not publish empty translations from Crowdin

--- download-translations.sh
+++ download-translations.sh
@@ -6,3 +6,16 @@ curl http://api.crowdin.net/api/project/learn-to-use-bitcoin/export?key=${key}
 wget http://api.crowdin.net/api/project/learn-to-use-bitcoin/download/all.zip?key=${key} -O all.zip
 unzip -o all.zip -d config/locales/
 rm all.zip
+cd config/locales/
+list=`find . -name "strings.yml"`
+for file in ${list}; do
+  directory=`dirname ${file}`
+  if [ -d ${directory} ] && [ ! `echo ${directory} | grep "en"` ]; then
+    echo "Comparing ${directory} with english"
+    if [ `diff ${file} en/strings.yml | wc -l` -lt 50 ]; then
+      echo "${directory} will be removed because it doesn't contain enough translated texts"
+      rm -R ${directory}
+    fi
+  fi
+done
+cd -
