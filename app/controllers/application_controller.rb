1d1f57ab66b7751811d9ab680a55f5d3670afef3
Sebas <neoranga55@gmail.com>
2014-08-25 17:25:54 +0000

    Add manual selection of page language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+  
+  private
+  def set_locale
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+    if params[:locale] # Manually select language with URL ruquest parameter, e.g. /?locale=en
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      elsif I18n.available_locales.include?(params[:locale].split('-')[0].to_sym)
+        I18n.locale = params[:locale].split('-')[0] # Try with generic language, e.g. select 'en' if 'en-US' is selected
+      end
+    end
+  end  
 end
