60effb74c9f0d545233568ebd1dbb5d10c089f33
Sebas <neoranga55@gmail.com>
2014-09-28 15:48:00 +0000

    Add send BTC for a Blockchain.info wallet and Coinbase exchange rates

--- app/models/blockchaindotinfo.rb
+++ app/models/blockchaindotinfo.rb
@@ -7,13 +7,13 @@ class Blockchaindotinfo
   
   attr_reader :guid, :link
   
-  def initialize( password, private_key_to_import, address_label = nil, email = nil)
+  def initialize( private_key_to_import, address_label = nil, password = nil, email = nil)
     
-    @password = password
+    @password = password ? password : ENV['DEFAULT_BLOCKCHAINDOTINFO_PASSWORD']
     api_key = ENV['BLOCKCHAINDOTINFO_API_KEY']
-    request_url = build_URL('/api/v2/create_wallet')
+    request_url = Blockchaindotinfo.build_URL('/api/v2/create_wallet')
     # build the params string
-    post_args = { password: password, api_code: api_key, priv: private_key_to_import }
+    post_args = { password: @password, api_code: api_key, priv: private_key_to_import }
     post_args[:email] = email if email
     post_args[:label] = address_label if address_label
     # send the request
@@ -34,13 +34,61 @@ class Blockchaindotinfo
     end
   end
   
-  def self.c_test
+  def self.send_money(guid, dest_address, amount, fee = nil, password = nil)
+    request_url = Blockchaindotinfo.build_URL( "/merchant/#{guid}/payment" )
+    password = password ? password : ENV['DEFAULT_BLOCKCHAINDOTINFO_PASSWORD']
+    # build the params string
+    amount = (amount / Float(ENV['SATOSHI'])).to_i # Set amount to Satoshis for Blockchain.info API
+    
+    post_args = { password: password, to: dest_address, amount: amount }
+    post_args[:fee] = fee if (fee) && (fee.to_f > 0)
+    # send the request
+    resp, data = Net::HTTP.post_form( URI.parse( request_url ), post_args )
+    puts resp
+    puts resp.header
+    puts resp.body
+    if resp.code.to_i != 200
+      Rails.logger.error "#{GlobalConstants::DEBUG_MSG}-#{cn}: Error sending money to #{dest_address} in Blockchain.info wallet guid #{guid}. Response:#{resp.body if resp}"
+      raise Exceptions::BlockchainDotInfoWalletSendMoneyError, "Response from Blockchain.info was #{resp.code} when calling URL #{request_url}"
+    end
+    if (! resp) || (! resp.body) || (resp.body.size == 0)
+      raise Exceptions::BlockchainDotInfoWalletSendMoneyError, "Response from Blockchain.info was empty calling URL #{request_url}"
+    end
+    begin
+      JSON.parse( resp.body )
+    rescue JSON::ParserError
+      raise Exceptions::BlockchainDotInfoWalletSendMoneyError, "Error parsing JSON response from Blockchain.info, response was:#{resp.body if resp}"
+    end
+  end
+  
+  
+  
+  def self.send_test
+    guid = 'eccb6fe8-9ac6-41e9-867e-f523dca7c4d9'
+    dest_address = '1Jtyqx79yWRjQmTQTbhHysm1tx953cJcMV'
+    amount = 0.0001
+    Blockchaindotinfo.send_money(guid, dest_address, amount)
+  end  
+  
+  def self.create_test
     private_key_to_import = '5KLTJs1vWcBjCWgNAfTpstNdSvuKZvnDouoVx2BET2JmAaeWt47' # Real test
     address_label = 'First free bitcoins from www learntousebitcoin com  Do not use this address to receive money'
-    Blockchaindotinfo.new('learntousebitcoin.com', private_key_to_import, address_label)
+    Blockchaindotinfo.new(private_key_to_import, address_label)
   end
 
 
+  def self.get_BTC_price( currency = 'USD' )
+    exchange_rate = nil
+    begin
+      cb_bank = Money::Bank::Coinbase.new
+      cb_bank.fetch_rates! # Call this before calculating exchange rates, it will download the rates from CoinBase
+      exchange_rate = cb_bank.exchange_with(Money.new(1000000, :BTC), currency.to_sym).fractional # Price of 1 BTC in currency
+    rescue => e
+      Rails.logger.warn "#{GlobalConstants::DEBUG_MSG}-#{cn}: Error getting exchange rate from Coinbase. Error message: #{e.message} - Response:#{resp.body if resp}"
+    end
+    return exchange_rate
+  end
+  
   private
   def parse_new_wallet_JSON( response )
     data = JSON.parse( response )
@@ -53,8 +101,7 @@ class Blockchaindotinfo
   end
   
   def generate_address( label = nil )
-    api_key = ENV['BLOCKCHAINDOTINFO_API_KEY']
-    request_url = build_URL( "/merchant/#{@guid}/new_address" )
+    request_url = Blockchaindotinfo.build_URL( "/merchant/#{@guid}/new_address" )
 
     # build the params string
     post_args = { password: @password }
@@ -69,7 +116,7 @@ class Blockchaindotinfo
     end
   end
   
-  def build_URL( handler )
+  def self.build_URL( handler )
     ENV['BLOCKCHAINDOTINFO_API_URL'] + handler
   end
   
--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -2,4 +2,5 @@ module Exceptions
   class NoFreeWalletsAvailable < StandardError; end
   class NoVisitorError < StandardError; end
   class BlockchainDotInfoWalletCreationError < StandardError; end
+  class BlockchainDotInfoWalletSendMoneyError < StandardError; end
 end
