a2dfb3c8c8db77333c791ca6a1d216e2b2e862b7
neoranga <neoranga55@gmail.com>
2014-11-08 18:39:22 +0100

    Improve static analysis problems in Ruby Visitors Controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,40 +6,15 @@ class VisitorsController < ApplicationController
  
   def show
     Rails.logger.debug "#{GlobalConstants::DEBUG_MSG}-#{cn}: Good, received email #{params[:email]}" ######################################################## TODO: Test code
-    search = Visitor.where(ip: request.remote_ip)
     
-    if search.empty?
-      # Associate visitor to wallet and show new wallet
-      Rails.logger.debug "#{GlobalConstants::DEBUG_MSG}-#{cn}: Good, search for IP #{request.remote_ip} didn't find any previous visitor in DB" ######################################################## TODO: Test code
-      @visitor = Visitor.new(ip: request.remote_ip)
-      
-      begin
-        @wallet = Wallet.associate_with_visitor!(@visitor)
-        if @wallet
-          # TODO: Register event for statistics
-          Rails.logger.debug "#{GlobalConstants::DEBUG_MSG}-#{cn}: Visitor with new wallet saved in DB" ######################################################## TODO: Test code
-          render action: 'show', :layout => false
-        else
-          show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error linking wallet to visitor in DB",
-            "Our monkeys made a mess again and created a magic error linking wallet to visitor in DB", true)
-        end
-      rescue ActiveRecord::ActiveRecordError => e
-        show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error, Not possible to interact with DB. Error:#{e}-#{e.message}",
-            "Our monkeys made a mess and could not access the database. Contact invitations@leartousebitcoin.com", true)
-      rescue Exceptions::NoVisitorError
-        show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error, Not possible to create a new Visitor (recieved nil from call to new)",
-            "Our monkeys made a mess and could not create a new record for you, our precious visitor. Contact invitations@leartousebitcoin.com", true)
-      rescue Exceptions::NoFreeWalletsAvailable
-        flash_message = "Our monkeys discovered there are no more wallets in the DB, \
-wait until the master monkey refills it and come back or \
-contact her directly at invitations@leartousebitcoin.com" # TODO: Improve message and UX flow
-        show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error, no available wallets in DB", flash_message, true)
-      end
-    else # Do not show wallet
+    if is_associated? # Do not show wallet
       # TODO: proper render of message or page
+      # TODO improve messaging and UX flow
       show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Not showing again wallet of existing visitor",
-          "Our super monkeys detected that you already received a wallet with some Bitcoins before", # TODO improve messaging and UX flow
+          "Our super monkeys detected that you already received a wallet with some Bitcoins before",
           true)
+    else
+      associate_wallet_and_visitor()
     end
   end 
          
@@ -75,5 +50,38 @@ contact her directly at invitations@leartousebitcoin.com" # TODO: Improve messag
     Rails.logger.error log_error_message if log_error_message
     flash[:error] = flash_error_message if flash_error_message
     redirect_to root_path if redirect_to_root # halts request cycle
+  end
+
+  def is_associated?
+    return ! (Visitor.where(ip: request.remote_ip)).empty?
+  end
+
+  def associate_wallet_and_visitor()
+    # Associate visitor to wallet and show new wallet
+    Rails.logger.debug "#{GlobalConstants::DEBUG_MSG}-#{cn}: Good, search for IP #{request.remote_ip} didn't find any previous visitor in DB" ######################################################## TODO: Test code
+    @visitor = Visitor.new(ip: request.remote_ip)
+    
+    begin
+      @wallet = Wallet.associate_with_visitor!(@visitor)
+      if @wallet
+        # TODO: Register event for statistics
+        Rails.logger.debug "#{GlobalConstants::DEBUG_MSG}-#{cn}: Visitor with new wallet saved in DB" ######################################################## TODO: Test code
+        render action: 'show', :layout => false
+      else
+        show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error linking wallet to visitor in DB",
+          "Our monkeys made a mess again and created a magic error linking wallet to visitor in DB", true)
+      end
+    rescue ActiveRecord::ActiveRecordError => e
+      show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error, Not possible to interact with DB. Error:#{e}-#{e.message}",
+          "Our monkeys made a mess and could not access the database. Contact invitations@leartousebitcoin.com", true)
+    rescue Exceptions::NoVisitorError
+      show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error, Not possible to create a new Visitor (recieved nil from call to new)",
+          "Our monkeys made a mess and could not create a new record for you, our precious visitor. Contact invitations@leartousebitcoin.com", true)
+    rescue Exceptions::NoFreeWalletsAvailable
+      flash_message = "Our monkeys discovered there are no more wallets in the DB, \
+wait until the master monkey refills it and come back or \
+contact her directly at invitations@leartousebitcoin.com" # TODO: Improve message and UX flow
+      show_error("#{GlobalConstants::DEBUG_MSG}-#{cn}: Error, no available wallets in DB", flash_message, true)
+    end
   end
 end
